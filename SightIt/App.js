54bb7e40477b8c635423a80e102f25f6113b83d5
EduardRoque13 <125721249+EADRoque@users.noreply.github.com>
2024-02-07 17:48:25 +0800

    camera-update

--- SightIt/App.js
+++ SightIt/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Camera } from 'expo-camera';
 
 const Stack = createNativeStackNavigator();
 
@@ -43,9 +44,51 @@ const HomeScreen = ({ navigation }) => {
 };
 
 const MapTrainingScreen = ({ navigation, route }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef(null);
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      try {
+        const { status } = await Camera.requestCameraPermissionsAsync();
+        if (status === 'granted') {
+          const photo = await cameraRef.current.takePictureAsync();
+          console.log(photo.uri);
+          // You can do something with the captured photo URI here
+        } else {
+          console.log('Permission denied');
+        }
+      } catch (error) {
+        console.error('Error taking picture:', error);
+      }
+    }
+  };
+
+  const checkCameraPermission = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setHasPermission(status === 'granted');
+  };
+
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Map Training Screen</Text>
+      {hasPermission === null ? (
+        <Text>Requesting camera permission...</Text>
+      ) : hasPermission === false ? (
+        <Text>No access to camera</Text>
+      ) : (
+        <Camera
+          style={styles.camera}
+          type={Camera.Constants.Type.back}
+          ref={cameraRef}
+        />
+      )}
+      <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+        <Text style={styles.captureButtonText}>Take Picture</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -78,6 +121,24 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
     fontSize: 25,
+  },
+  captureButton: {
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    padding: 15,
+    borderRadius: 10,
+  },
+  captureButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  camera: {
+    height: 250,
+    width: 320,
+    marginTop: -300
   },
 });
 
