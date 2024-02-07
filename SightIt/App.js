316dceabb138fae2024cedb8891fe53f206d6cdc
EduardRoque13 <125721249+EADRoque@users.noreply.github.com>
2024-02-07 16:33:27 +0800

    MapTrainingPageAdded

--- SightIt/App.js
+++ SightIt/App.js
@@ -1,8 +1,30 @@
-import { Alert } from 'react-native';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Map_Training'
+          component={MapTrainingScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Image
@@ -11,16 +33,23 @@ export default function App() {
       />
       <TouchableOpacity
         style={styles.button}
-        onPress={() => Alert.alert('SML')}
+        onPress={() => navigation.navigate('Map_Training')}
       >
-        <Text
-          style={styles.text}
-        >Create Map</Text>
+        <Text style={styles.text}>Create Map</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+const MapTrainingScreen = ({ navigation, route }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Map Training Screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -33,7 +62,7 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
     resizeMode: 'contain',
-    margin: 130
+    margin: 10, 
   },
   button: {
     backgroundColor: '#CF0A2C',
@@ -41,12 +70,15 @@ const styles = StyleSheet.create({
     height: 70,
     borderRadius: 40,
     justifyContent: 'center',
-    alignContent: 'center'
+    alignItems: 'center', 
+    marginTop: 20, 
   },
   text: {
     textAlign: 'center',
     color: 'white',
     fontWeight: 'bold',
-    fontSize: 25
-  }
+    fontSize: 25,
+  },
 });
+
+export default MyStack;
